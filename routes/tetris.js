2ff398b6353fe77d802b7fd3224edf43c6c0d298
刘巧 <liuqiao@liuqiaodeMacBook-Pro.local>
2017-10-27 16:14:49 +0800

    add tetris v1.0.0

--- routes/tetris.js
+++ routes/tetris.js
@@ -3,10 +3,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if(req.session.tetrisName){
+        console.log("--------玩家--------" + req.session.tetrisName + "--------------进来了-----------")
         res.render('tetris', { title: "俄罗斯方块", tetrisName: req.session.tetrisName });
     }else{
         res.redirect('/')
     }
-
 })
 module.exports = router;
